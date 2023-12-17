3e3599fe762d785a1fbb4adaa8b887bb57317618
Jhon Gomez <jhonjairogomez92@gmail.com>
2023-12-17 19:17:12 +0100

    Add: API consumtion to display data in Card component

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,25 @@
+import { useState, useEffect } from 'react';
 import { Layout } from '../../Components/Layout'
 import { Card } from '../../Components/Card';
+import { data } from 'autoprefixer';
 
 function Home () {
+  const [items, setItems] =useState(null);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then(response => response.json())
+      .then(data => setItems(data))
+  },[])
+
+  console.log(data)
   return (
     <Layout>
-      Home
-      <Card />
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {
+          items?.map(product =>(<Card key={product.id} {...product}/>))
+        }
+      </div>
     </Layout>
   )
 }
